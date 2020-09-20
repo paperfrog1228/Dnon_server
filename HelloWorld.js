d92124d6ac10535cf373b5dba3ca9a608e4255be
paperfrog <paperfrog@naver.com>
2020-09-21 03:12:15 +0900

    Test jenkins 배포는 잘 작동하지만, 단순 테스트용으로는 비추라고 하신다. 서버 주인님이.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+
+const http = require('http');
+ 
+const hostname = '0.0.0.0';
+const port = '8081';
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+   
+    res.end('Hello World111');
+    
+}).listen(port, hostname, 
+() => {    
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
